b27b411f730b0da7dc2532d2b54995aaf0fa3af5
newievong <143970329+newievong@users.noreply.github.com>
2023-10-30 21:18:59 -0400

    Update studentdata2.py

--- studentdata2.py
+++ studentdata2.py
@@ -8,44 +8,48 @@ def list_names(students):
     for peoplenum in range(len(students)):
         name = students[peoplenum]['name']
         print(f"{peoplenum + 1}. {name}")
-option = input("Please select which action you'd like to do: add, view, or quit: ")
+def get_new_student():
+    newinfo = {}
+    newinfo['name'] = input("Please input a name for the new student: ")
+    newinfo['hometown'] = input("Next please input their hometown: ")
+    newinfo['fav_food'] = input("Last, please input their favorite food: ")
+    return newinfo
 
-while option != 'add' and option != 'view' and option != 'quit':
-    option = input("This is an invalid input. Please try again by typing in: add, view, or quit:  ")
+while True:
+    option = input("Please select which action you'd like to do: add, view, or quit: ")
 
-if option == 'view':
-    list_names(allinfo)
-    choice = int(input(f"Which student would you like to learn more about? Enter a number between 1-{len(allinfo)}: "))
+    while option != 'add' and option != 'view' and option != 'quit':
+        option = input("This is an invalid input. Please try again by typing in: add, view, or quit:  ")
 
-
-    while choice > len(allinfo) or choice < 1:
-        choice = int(input("This is an invalid number. Please try again by entering a number between 1-4: "))
-    else:
-        selectstud = allinfo[choice - 1]
-        print(f"Student {choice} is {selectstud['name']}. What would you like to know?")
-        choice2 = input("Enter 'hometown' or 'favorite food': ")
-
-        while choice2 != 'hometown' and choice2 != 'favorite food':
-            choice2 = input("This is an invalid input. Please enter 'hometown' or 'favorite food: ")
-
-        if choice2 == 'hometown':
-            print(f"{selectstud['name']}'s hometown is {selectstud['hometown']}.")
+    if option == 'view':
+        list_names(allinfo)
+        choice = int(input(f"Which student would you like to learn more about? Enter a number between 1-{len(allinfo)}: "))
 
 
-        elif choice2 == 'favorite food':
-            print(f"{selectstud['name']}'s favorite food is {selectstud['fav_food']}.")
+        while choice > len(allinfo) or choice < 1:
+            choice = int(input("This is an invalid number. Please try again by entering a number between 1-4: "))
+        else:
+            selectstud = allinfo[choice - 1]
+            print(f"Student {choice} is {selectstud['name']}. What would you like to know?")
+            choice2 = input("Enter 'hometown' or 'favorite food': ")
+
+            while choice2 != 'hometown' and choice2 != 'favorite food':
+                choice2 = input("This is an invalid input. Please enter 'hometown' or 'favorite food: ")
+
+            if choice2 == 'hometown':
+                print(f"{selectstud['name']}'s hometown is {selectstud['hometown']}.")
 
 
-if option == 'add':
-    def get_new_student():
-        newinfo = {}
-        newinfo['name'] = input("Please input a name for the new student: ")
-        newinfo['hometown'] = input("Next please input their hometown: ")
-        newinfo['fav_food'] = input("Last, please input their favorite food: ")
-        return newinfo
-
-    allinfo.append(get_new_student())
+            elif choice2 == 'favorite food':
+                print(f"{selectstud['name']}'s favorite food is {selectstud['fav_food']}.")
 
 
-elif option == 'quit':
-  print("Goodbye!")
+    if option == 'add':
+        variable = get_new_student()
+        allinfo.append(variable)
+
+
+
+    elif option == 'quit':
+        print("Goodbye!")
+        break
